636fb691e913f49a6381bb46fd59aeea50b15680
Ronnie Jandou <31853940+rjandou@users.noreply.github.com>
2023-08-21 17:24:09 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const _rtf = require('./app/src/rtf/rtf.class');
 const rtf = new _rtf();
 
-function htmlToRtf(html) {
-    return rtf.convertHtmlToRtf(html);
+function htmlToRtf(html, escapeText = true) {
+    return rtf.convertHtmlToRtf(html, escapeText);
 }
 
 window.htmlToRtf = htmlToRtf;
